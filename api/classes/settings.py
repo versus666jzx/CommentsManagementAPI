906406d033bb0a415245553bdb5a0dcd80666b96
HardWorkingStation <HardWorkingStation@my_mail.flow>
2023-10-13 13:16:54 +0300

    Исправлен баг при котором сервис не запускался

--- api/classes/settings.py
+++ api/classes/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     ES_PASSWORD: str = Field(default="elastic")
     ES_USER: str = Field(default="elastic")
     ES_PROTO: str = Field(default="https")
-    ES_HOST: str = Field(default="localhost")
+    ES_HOST: str = Field(default="elastic")
     ES_PORT: str | int = Field(default="9200")
     ES_VERIFY_CERTS: bool = Field(default=False)
     # атрибуты
