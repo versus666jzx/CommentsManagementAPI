23685cc3890ecb865d5476177bfb70923f163cdc
va-akhmetov <ahmetov@itexp.p>
2023-10-13 12:55:54 +0300

    fix

--- api/classes/settings.py
+++ api/classes/settings.py
@@ -12,8 +12,5 @@ class Settings(BaseSettings):
     ES_HOST: str = Field(default="localhost")
     ES_PORT: str | int = Field(default="9200")
     ES_VERIFY_CERTS: bool = Field(default=False)
-    # поисковые настройки
-    ALL_ARTICLE_INDEXES: list[str] = ["ru_articles", "en_articles", "other_articles"]
-    ALL_COMMENTS_INDEXES: list[str] = ["ru_comments", "en_comments", "other_comments"]
     # атрибуты
-    DELETE_ALL_INDEXES_ON_STARTUP: bool = False
+    DELETE_ALL_INDEXES_ON_STARTUP: bool = Field(default=False)
