918c481b94240ea96c77c11c46e6f2228f4e6c7a
HardWorkingStation <hws@my.power>
2023-10-18 01:14:05 +0300

    Добавлены настройки подключения к PG

--- api/postgres_tools/postgres_connection.py
+++ api/postgres_tools/postgres_connection.py
@@ -1,10 +1,12 @@
 from psycopg2 import connect
 
+from dataclasses import dataclass
 from api.classes.settings import Settings
 
 settings = Settings()
 
 
+@dataclass
 class PGInstance:
     def __init__(self):
         self.pg_connect = connect(
@@ -12,8 +14,16 @@ class PGInstance:
             port=settings.PG_PORT,
             user=settings.PG_USER,
             password=settings.PG_PASSWORD,
-            dbname="public"
+            dbname="public",
+            autocommit=True,
+            options="-c statement_timeout=300000",
         )
+
+    def __post_init__(self):
+        self.pg_connect.autocommit = True
 
     def close_connection(self):
         self.pg_connect.close()
+
+
+pg_instance = PGInstance()
